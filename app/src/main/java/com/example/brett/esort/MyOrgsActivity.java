3938120b887ac486cbf5b3d9ae509ea96c767093
Brett Kercher <brettkercher@utexas.edu>
2015-11-17 20:44:47 -0600

    Logout

--- app/src/main/java/com/example/brett/esort/MyOrgsActivity.java
+++ app/src/main/java/com/example/brett/esort/MyOrgsActivity.java
@@ -1,5 +1,6 @@
 package com.example.brett.esort;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MyOrgsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -88,14 +91,21 @@ public class MyOrgsActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logout) {
+            doLogout();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void doLogout() {
+        ParseUser.logOut();
+        Intent intent = new Intent(MyOrgsActivity.this, DispatchActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
