e6bdb577164add7fc294ec51e55d04c73edf7dfc
mbelyaeva <maria123belyaeva@gmail.com>
2015-11-19 15:13:17 -0600

    Started Alerts

---
+++ app/src/main/java/com/example/brett/esort/popUp.java
@@ -0,0 +1,21 @@
+package com.example.brett.esort;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+/**
+ * Created by Maria on 11/19/2015.
+ */
+public class popUp extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+
+    }
+}
