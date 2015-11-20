58cdc57959cb3185802168bfd8b2c6834fd5bc6a
Brett Kercher <brettkercher@utexas.edu>
2015-11-19 22:46:06 -0600

    Make a class with no join code

--- app/src/main/java/com/example/brett/esort/PopUpFragment.java
+++ app/src/main/java/com/example/brett/esort/PopUpFragment.java
@@ -6,7 +6,17 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Created by Maria on 11/19/2015.
@@ -14,6 +24,7 @@ import android.view.LayoutInflater;
 public class PopUpFragment extends DialogFragment {
 
     private String type;
+    EditText inputText;
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -21,11 +32,14 @@ public class PopUpFragment extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
         Bundle typeBundle = getArguments();
-        String type = typeBundle.getString("type");
+        final String type = typeBundle.getString("type");
         int layout_id;
 
         if(type == null) {
+            //close dialog
             layout_id = R.layout.popup_join;
+            Toast.makeText(getActivity().getApplicationContext(), "Something went wrong",
+                    Toast.LENGTH_LONG).show();
         }
         else if(type.equals("join")) {
             layout_id = R.layout.popup_join;
@@ -41,7 +55,21 @@ public class PopUpFragment extends DialogFragment {
         builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id) {
-                //
+                // 1. Read Class Name from edit text
+                // 2. Generate a (Unique) Join Code
+                // 3. Make Organization Class with Name and Join Code
+                // 4. Tell Parse
+                // 5. Exit Dialogue
+                // 6. Show Join code
+                if(type.equals("make")) {
+                    make(dialog);
+                }
+                else if(type.equals("join")) {
+                    join(dialog);
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), "Something went wrong",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
@@ -52,5 +80,43 @@ public class PopUpFragment extends DialogFragment {
 
 
         return builder.create();
+    }
+
+    public void make(DialogInterface dialog){
+        inputText = (EditText) ( (AlertDialog)dialog).findViewById(R.id.orgName);
+        ParseObject org = new ParseObject("Organization");
+        org.put("name", inputText.getText().toString());
+        org.put("code", 1234);
+        org.saveInBackground();
+    }
+
+    public void join(DialogInterface dialog){
+        inputText = (EditText) ( (AlertDialog)dialog ).findViewById(R.id.joinCode);
+        //check that this passcode exists
+        System.out.println("blah");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Organization");
+        query.whereEqualTo("code", Integer.valueOf(inputText.getText().toString()));
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                System.out.println("doneeeeee");
+                if(objects != null && objects.size() > 0){
+                    //add person to the class
+                    ParseObject myOrg = objects.get(0);
+
+
+//                    Toast.makeText(getActivity().getApplicationContext(), "cats :3 meowwww",
+//                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    //Toast.makeText(getActivity().getApplicationContext(), "Sorry, that doesn't match anything we have.",
+                    //        Toast.LENGTH_LONG).show();
+                }
+//                listener.getTagsByNameCallback(tagname, objects);
+            }
+        });
+
+
+
     }
 }
